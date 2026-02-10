412469d4d800d103732433bc8f814f732d3fd14f
kumaresan <kumaresan.devops1990@gmail.com>
2026-02-10 20:30:49 +0000

    Rebase branch commit

--- script2.sh
+++ script2.sh
@@ -4,4 +4,8 @@ echo Main branch change
 =======
 echo Rebase update
 >>>>>>> 5020dae (Rebase branch commit)
+<<<<<<< HEAD
 echo Main branch change1
+=======
+echo Rebase update 1
+>>>>>>> 6cca44c (Rebase branch commit)
